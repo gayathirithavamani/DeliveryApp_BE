53b13f3fe2864a105bf94b1fb073665b5e9d3f8f
gayathirithavamani <gayat2000@gmail.com>
2023-06-20 15:14:30 +0530

    final

--- server.js
+++ server.js
@@ -1,40 +1,34 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-const connectDB = require('./config/config');
+const connectDB = require("./config/config");
 
-require('colors');
-const morgan = require('morgan');
+require("colors");
+const morgan = require("morgan");
 
 //config dotenv
-dotenv.config()
-
+dotenv.config();
 
 //connection mongodb
 connectDB();
-
 
 const app = express();
 
 //middleware
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(cors({ origin: "https://deliveryapp-fe.netlify.app"}))
-
-
+app.use(morgan("dev"));
+app.use(cors({ origin: "https://beamish-peony-8984d1.netlify.app" }));
 
 //route
 app.use("/api/burgers", require("./routes/burgerRoutes"));
-app.use("/api/users", require('./routes/userRoutes'));
-app.use("/api/orders", require('./routes/orderRoutes'))
-app.get("/",(req,res) =>{
-    res.send('<h1>Running Successfully</h1>');
-})
-
-
-const port =process.env.PORT || 5000;
-app.listen(port, ()=>{
-    console.log(`App listening on port ${port}!`)
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+app.get("/", (req, res) => {
+  res.send("<h1>Running Successfully</h1>");
 });
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
